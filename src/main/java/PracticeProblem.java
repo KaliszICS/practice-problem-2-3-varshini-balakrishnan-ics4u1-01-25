30927b4885868dffa46f79eafed8e9caff58451c
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-03 18:22:15 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,20 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static void bubbleSortString(String[] strings) {
+		boolean swapped;
+		for (int j = 0; j < strings.length - j; j++) {
+			swapped = false;
+			for (int i = 1; i < strings.length - j; i++) {
+				if (strings[i-1].compareToIgnoreCase(strings[i]) > 0) {
+					String temp = strings[i-1]; 
+					strings[i-1] = strings[i];
+					strings[i] = temp;
+					swapped = true;
+				}
+			}
+			if(!swapped) {
+				break;
+			}
+		}
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
